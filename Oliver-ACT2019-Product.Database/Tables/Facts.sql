f65508daf13ccb9e53a3bf5f48dc261844c19989
Oliver <oliver@kevinandcarly.com>
2019-04-30 20:53:47 -0400

    Facts table changes

--- Oliver-ACT2019-Product.Database/Tables/Facts.sql
+++ Oliver-ACT2019-Product.Database/Tables/Facts.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Facts]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Fact] VARCHAR(50) NOT NULL, 
-    [Park_Id] INT NOT NULL
+    [Fact] VARCHAR(500) NOT NULL, 
+    [Park_Id] INT NULL
 )
