2cdec4a02070dc1bc30976711114dbbd98fac9b4
Oliver <oliver@kevinandcarly.com>
2019-05-14 20:15:31 -0400

    fixed it

---
+++ Oliver-ACT2019-Product.Web/Pages/Glossary.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Oliver_ACT2019_Product.Web.Pages
+{
+    public class GlossaryModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
